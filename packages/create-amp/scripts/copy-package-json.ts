53702a520a55eb9dd8c97684045b25cc9201c4fa
Chris Whited <cmwhited1990@gmail.com>
2025-11-13 15:50:35 -1000

    feat(create amp): first publish. create changelog. copy license, changelog and readme to dist on publish

--- packages/create-amp/scripts/copy-package-json.ts
+++ packages/create-amp/scripts/copy-package-json.ts
@@ -34,10 +34,34 @@ const write = (pkg: object) =>
     Effect.flatMap((fileSystem) => fileSystem.writeFileString(pathTo, JSON.stringify(pkg, null, 2)))
   )
 
+const copyFiles = FileSystem.FileSystem.pipe(
+  Effect.flatMap((fileSystem) =>
+    Effect.all([
+      fileSystem.readFileString("README.md").pipe(
+        Effect.flatMap((content) => fileSystem.writeFileString(path.join("dist", "README.md"), content)),
+        Effect.tap(() => Effect.log("Copied README.md to dist/"))
+      ),
+      fileSystem.readFileString("CHANGELOG.md").pipe(
+        Effect.flatMap((content) => fileSystem.writeFileString(path.join("dist", "CHANGELOG.md"), content)),
+        Effect.tap(() => Effect.log("Copied CHANGELOG.md to dist/"))
+      ),
+      fileSystem.readFileString("llms.txt").pipe(
+        Effect.flatMap((content) => fileSystem.writeFileString(path.join("dist", "llms.txt"), content)),
+        Effect.tap(() => Effect.log("Copied llms.txt to dist/"))
+      ),
+      fileSystem.readFileString("../../LICENSE").pipe(
+        Effect.flatMap((content) => fileSystem.writeFileString(path.join("dist", "LICENSE"), content)),
+        Effect.tap(() => Effect.log("Copied LICENSE to dist/"))
+      )
+    ])
+  )
+)
+
 const program = pipe(
   Effect.log(`Copying package.json to ${pathTo}...`),
   Effect.zipRight(read),
   Effect.flatMap(write),
+  Effect.zipRight(copyFiles),
   Effect.provide(NodeFileSystem.layer)
 )
 
